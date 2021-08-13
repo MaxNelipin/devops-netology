6bfb26bb0b7011dd162928673f1d6ecd9dc0e87f
maxim.nelipin <nelmaxim@gmail.com>
2021-08-13 09:52:52 +0300

    Задание 4.3 - структура на несколько хостов

--- 4.3_JSON_YAML/2.py
+++ 4.3_JSON_YAML/2.py
@@ -5,32 +5,42 @@ import time
 import json
 import yaml
 
-hostname = "mail.google.com"
+arHost = [{"mail.google.com": ""}, {"www.google.com": ""}]
+
 port = 443
 prevIP = ""
 a = 0
+
 while a < 15:
-    currentIP = socket.gethostbyname(hostname)
-    if currentIP != prevIP:
-        print(f"[ERROR] {hostname} IP mismatch: previous: {prevIP} new: {currentIP}")
-        prevIP = currentIP
+    # Флаг изменения IP
+    changeIP = False
+    for i in range(len(arHost)):
+        for hostname, prevIP in arHost[i].items():
+            currentIP = socket.gethostbyname(hostname)
+            if currentIP != prevIP:
+                print(f"[ERROR] {hostname} IP mismatch: previous: {prevIP} new: {currentIP}")
+                prevIP = currentIP
+                arHost[i][hostname] = currentIP
+                changeIP = True
+            else:
+                print(f"{hostname} - {currentIP}")
+            # Проверка доступности
+            conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                conn.connect((hostname, port))
+            except socket.error:
+                print("Not available", "\n-----------------------------------------")
+            else:
+                print("Available", "\n-----------------------------------------")
+            finally:
+                conn.close()
+    # Если IP менялись, то пишем новую структуру в файлы
+    if changeIP:
         with open('2.yaml', 'w') as ym:
-            ym.write(yaml.dump([{hostname: currentIP}]))
+            ym.write(yaml.dump(arHost))
             ym.close()
         with open('2.json', 'w') as js:
-            js.write(json.dumps({hostname: currentIP}))
+            js.write(json.dumps(arHost))
             js.close()
-    else:
-        print(f"{hostname} - {currentIP}")
-    # Проверка доступности
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        conn.connect((hostname, port))
-    except socket.error:
-        print("Not available", "\n-----------------------------------------")
-    else:
-        print("Available", "\n-----------------------------------------")
-    finally:
-        conn.close()
     a += 1
-    time.sleep(15)
+    time.sleep(5)
